06572af3f02cbc51b5066ed7aaafad7324a56abe
Taylor Treece <taylor.treece@protonmail.com>
2021-02-04 11:57:23 -0600

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,53 @@
+class Board
+
+    attr_accessor :cells
+
+    @cells = []
+
+    def reset!
+        self.cells = Array.new(9, " ")
+    end
+
+    def initialize
+        reset!
+    end
+
+    def display
+        puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+        puts "-----------"
+        puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+        puts "-----------"
+        puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+    end
+
+    def position(user_input)
+        self.cells[user_input.to_i - 1]
+    end
+
+    def full?
+        self.cells.all? {|cell| cell == "X" || cell == "O"}
+    end
+
+    def turn_count
+        turn = 0
+        self.cells.each do |space|
+            if space == "X" || space == "O"
+               turn += 1
+            end
+        end
+        turn 
+    end
+
+    def taken?(user_input)
+        position(user_input) == "X" || position(user_input) == "O" || position(user_input) != " "
+    end
+
+    def valid_move?(user_input)
+        user_input.to_i.between?(1,9) && !taken?(user_input)
+    end
+
+    def update(user_input, player)
+        self.cells[user_input.to_i-1] = player.token #Looking forward, I think this will be a method you create in the player section
+    end
+
+end
